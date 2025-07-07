0300b4fc73f34c401e0e0ff18b73cf91477ac64c
Childish <cedric.belin1@gmail.com>
2025-07-07 11:30:39 +0200

    change folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NavigationModule } from './navigation/navigation.module';
-import { Navigation } from './navigation/entities/navigation.entity';
+import { NavigationModule } from './domains/navigation/navigation.module';
+import { Navigation } from './domains/navigation/entities/navigation.entity';
 
 
 @Module({
