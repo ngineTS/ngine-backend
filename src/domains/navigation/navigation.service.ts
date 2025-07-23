e411516adb8a666c10a84e5994ef52d1a3d93fd0
Childish <cedric.belin1@gmail.com>
2025-07-23 13:08:29 +0200

    add TODO comment

--- src/domains/navigation/navigation.service.ts
+++ src/domains/navigation/navigation.service.ts
@@ -29,6 +29,7 @@ export class NavigationService {
       deletedDate: IsNull(),
       parentId: IsNull() 
     };
+    //TO DO: Replace 4 by the exact depth wished
     this.generateNestedRelationsOrderAndFilters(4, relations, order, where);
     return await this.navigationRepository.find({
       relations: [...relations],
