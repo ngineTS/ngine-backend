6e008947c2bd20bd1c640f0df3a8e730a3714e9e
Childish <cedric.belin1@gmail.com>
2025-08-15 14:04:57 +0200

    define entity schema

--- src/domains/custom-form/entities/custom-form.entity.ts
+++ src/domains/custom-form/entities/custom-form.entity.ts
@@ -1,1 +1,24 @@
-export class CustomForm {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class CustomForm {
+
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    tableId: string;
+
+    @Column()
+    columnName: string;
+
+    @Column()
+    inputType: string;
+
+    @Column()
+    inputLabel: string;
+
+    @Column()
+    validators: Array<string>;
+    
+}
--- src/domains/table-viz/entities/table-viz.entity.ts
+++ src/domains/table-viz/entities/table-viz.entity.ts
@@ -1,1 +1,15 @@
-export class TableViz {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class TableViz {
+
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    navigationId: string;
+
+    @Column()
+    tableName: string;
+
+}
