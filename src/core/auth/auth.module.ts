7b0d9454bac2b3903bae2af6f89f8cd3ef1daf5d
Childish <cedric.belin1@gmail.com>
2025-10-19 00:29:56 +0200

    implement refresh logic final

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { AuthGuard } from './auth.guard';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '5m' },
+      signOptions: { expiresIn: '15m' },
     }),
     TypeOrmModule.forFeature([User, PasswordRecovery])
   ],
