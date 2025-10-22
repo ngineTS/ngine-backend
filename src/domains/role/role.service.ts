2f03e79ca45702c8279488867b068281de36bdfb
Childish <cedric.belin1@gmail.com>
2025-10-22 17:28:14 +0200

    implement role controller APIs

--- src/domains/role/role.service.ts
+++ src/domains/role/role.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from './entities/role.entity';
+import { IsNull, Repository } from 'typeorm';
+import { UserRole } from '../user-role/entities/user-role.entity';
+import { RoleNavigationPermission } from '../role-navigation-permission/entities/role-navigation-permission.entity';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+
+  constructor(@InjectRepository(Role)
+              private _roleRepository: Repository<Role>,
+              @InjectRepository(UserRole)
+              private _userRoleRepository: Repository<UserRole>,
+              @InjectRepository(RoleNavigationPermission)
+              private _roleNavigationPermissionRepository: Repository<RoleNavigationPermission>) {}
+
+  async create(createRoleDto: CreateRoleDto) {
+    return await this._roleRepository.save(createRoleDto);
   }
 
-  findAll() {
-    return `This action returns all role`;
+  async findAll() {
+    return await this._roleRepository.find({
+      relations: [
+        'roleNavigationPermissions',
+        'roleNavigationPermissions.navigation',
+        'roleNavigationPermissions.permission'
+      ],
+      where: { deletedDate: IsNull() }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async update(id: string, updateRoleDto: UpdateRoleDto) {
+    updateRoleDto["updatedDate"] = new Date();
+    updateRoleDto["updatedBy"] = '00000000-0000-0000-0000-000000000000';
+    return await this._roleRepository.update(id, updateRoleDto);
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
+  /**
+   * Remove role and his relations.
+   * @param id The role id to remove.
+   */
+  async remove(id: string) {
+    let removedTotal = 0;
+    const updateRoleResponse = await this._roleRepository.update(id, {
+      deletedDate: new Date(),
+      deletedBy: '00000000-0000-0000-0000-000000000000'
+    });
+    
+    if (updateRoleResponse && updateRoleResponse.affected === 0) {
+      throw new NotFoundException();
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+    let userRolesToRemove = await this._userRoleRepository.find({
+      where: { roleId: id }
+    });
+    for(let userRole of userRolesToRemove) {
+      userRole["deletedDate"] = new Date();
+      userRole["deletedBy"] = '00000000-0000-0000-0000-000000000000';
+    }
+    removedTotal = removedTotal 
+      + (await this._userRoleRepository.save(userRolesToRemove)).length;
+
+    let roleNavigationPermissionsToDelete = await this._roleNavigationPermissionRepository.find({
+      where: { roleId: id }
+    });
+    for(let roleNavigationPermission of roleNavigationPermissionsToDelete) {
+      roleNavigationPermission["deletedDate"] = new Date();
+      roleNavigationPermission["deletedBy"] = '00000000-0000-0000-0000-000000000000';
+    }
+    removedTotal = removedTotal 
+      + (await this._userRoleRepository.save(roleNavigationPermissionsToDelete)).length;    
+
+    return removedTotal + updateRoleResponse.affected!;
   }
 }
