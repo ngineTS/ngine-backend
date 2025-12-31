267df29f33942715869ce25782a608e8d01ced36
Childish <cedric.belin1@gmail.com>
2025-12-31 15:51:27 +0100

    role guard table viz controller

--- src/domains/table-viz/table-viz.controller.ts
+++ src/domains/table-viz/table-viz.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UseGuards } from '@nestjs/common';
 import { TableVizService } from './table-viz.service';
 import { CreateTableVizDto } from './dto/create-table-viz.dto';
 import { UpdateTableVizDto } from './dto/update-table-viz.dto';
+import { Feature, Permission } from 'src/core/decorators/role.decorator';
+import { RolesGuard } from 'src/core/guards/role.guard';
 
+@Feature('calendar')
 @Controller('table-viz')
 export class TableVizController {
   
   constructor(private readonly tableVizService: TableVizService) {}
 
   @Post()
+  @Permission('add')
+  @UseGuards(RolesGuard)
   create(@Body() createTableVizDto: CreateTableVizDto) {
     return this.tableVizService.create(createTableVizDto);
   }
 
   @Get('navigation/:navigationId')
+  @Permission('view')
+  @UseGuards(RolesGuard)
   findByNavigationId(@Param('navigationId') navigationId: string) {
     return this.tableVizService.findByNavigationId(navigationId);
   }
@@ -28,6 +35,8 @@ export class TableVizController {
     return this.tableVizService.findTableContentByTableName(tableName);
   }
 
+  @Permission('edit')
+  @UseGuards(RolesGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTableVizDto: UpdateTableVizDto) {
     return this.tableVizService.update(id, updateTableVizDto);
