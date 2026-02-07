b5e13c72e320209dd6754548818e3357f09b2f47
Childish <cedric.belin1@gmail.com>
2026-02-07 21:48:23 +0100

    ad

--- src/domains/file-management/file-management.service.ts
+++ src/domains/file-management/file-management.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 import path = require('path');
@@ -15,26 +15,49 @@ export class FileManagementService {
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
 
+  /**
+   * Upload file and save media record with file key.
+   * @param file The file to upload.
+   * @param userId The userId from the request token (used for audit).
+   * @returns The file information saved.
+   */
   async uploadFile(file, userId: string) {
     const { originalname } = file;
-    const s3Response = await this.s3_upload(
-      file.buffer,
-      process.env.AWS_S3_BUCKET_NAME,
-      originalname,
-      file.mimetype,
-    );
-    const media: CreateMediaDto = {
-      name: s3Response.Key,
-      displayName: originalname,
-      type: file.mimetype,
-      createdBy: userId,
-      createdDate: new Date(),
-      updatedBy: userId,
-      updatedDate: new Date()
+
+    try {
+      const s3Response = await this.s3_upload(
+        file.buffer,
+        process.env.AWS_S3_BUCKET_NAME,
+        originalname,
+        file.mimetype,
+      );
+
+      const media: CreateMediaDto = {
+        name: s3Response.Key,
+        displayName: originalname,
+        type: file.mimetype,
+        createdBy: userId,
+        createdDate: new Date(),
+        updatedBy: userId,
+        updatedDate: new Date()
+      }
+
+      return await this.mediaService.create(media);
+    } 
+    catch (error) {
+      throw new BadRequestException(error);
     }
-    return await this.mediaService.create(media);
   }
 
+  /**
+   * Upload file to S3 bucket.
+   * 
+   * @param file The file to upload.
+   * @param bucket The bucket name.
+   * @param name The original file name.
+   * @param mimetype The file mime type.
+   * @returns Upload response from S3 API.
+   */
   async s3_upload(file, bucket, name, mimetype) {
     const fileName: string = path.parse(name).name + uuidv4() + path.parse(name).ext
     const params = {
@@ -42,33 +65,53 @@ export class FileManagementService {
       Key: fileName,
       Body: file,
       ContentType: mimetype
-      //ACL: 'public-read',
-      //ContentDisposition: 'inline',
-      //CreateBucketConfiguration: {
-      //  LocationConstraint: 'ap-south-1',
-      //},
-    };
+    }
+
     return await this.s3.upload(params).promise();
   }
 
+  /**
+   * Get S3 temporary file url (duration: 1 hour).
+   * 
+   * @param fileName The file key.
+   * @returns The temporary file url.
+   */
   getFile(fileName: string){
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: fileName,
-      Expires: 3600,
-    };
-    const url = this.s3.getSignedUrl('getObject', params);
-    return JSON.stringify(url);
+    try {
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: fileName,
+        Expires: 3600,
+      };
+      const url = this.s3.getSignedUrl('getObject', params);
+
+      return JSON.stringify(url);
+    } 
+    catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
+  /**
+   * Delete file from S3 bucket and soft delete media record.
+   * @param fileName The file key.
+   * @param userId The user id from request token (used for audit).
+   * @returns 'deleted'
+   */
   async deleteFile(fileName: string, userId){
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME!,
-      Key: fileName,
+    try {
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME!,
+        Key: fileName,
+      }
+  
+      await this.s3.deleteObject(params).promise();
+      await this.mediaService.softDelete(fileName, userId);
+      return JSON.stringify('deleted');
+    } 
+    catch(error) {
+      throw new BadRequestException(error);
     }
-    await this.s3.deleteObject(params).promise();
-    await this.mediaService.softDelete(fileName, userId);
-    return JSON.stringify('deleted');
   }
 
 }
