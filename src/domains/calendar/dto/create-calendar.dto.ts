337112fbd3ce6dd123da861982b350a10ea07c10
Childish <cedric.belin1@gmail.com>
2026-02-08 14:50:19 +0100

    implement calendar dto

--- src/domains/calendar/dto/create-calendar.dto.ts
+++ src/domains/calendar/dto/create-calendar.dto.ts
@@ -1,1 +1,40 @@
-export class CreateCalendarDto {}
+import { IsBoolean, IsDate, IsDateString, isNotEmpty, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+
+export class CreateCalendarDto {
+    
+    @IsNotEmpty()
+    @IsUUID()
+    navigationId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsDateString()
+    startDate: Date;
+
+    @IsNotEmpty()
+    @IsDateString()
+    endDate: Date;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    fileId?: string;
+
+    @IsOptional()
+    @IsString()
+    category?: string;
+
+    @IsOptional()
+    @IsString()
+    url?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    allDay?: boolean;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
     credentials: true
   });
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
