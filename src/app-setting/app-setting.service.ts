74c49f16cd40202387c979fce4b75fd228118ea6
Childish <cedric.belin1@gmail.com>
2026-03-31 23:01:10 +0200

    implement save app setting api

--- src/app-setting/app-setting.service.ts
+++ src/app-setting/app-setting.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AppSetting } from './entities/app-setting.entity';
 import { Repository } from 'typeorm';
+import { CreateAppSettingDto } from './dto/create-app-setting.dto';
 
 @Injectable()
 export class AppSettingService {
@@ -14,10 +15,23 @@ export class AppSettingService {
   /**
    * Find all app settings.
    * 
-   * @returns The app settings
+   * @returns The app settings.
    */
   findAll() {
     return this._appSettingRepository.find();
+  }
+
+
+  /**
+   *  Update app setting.
+   * 
+   * @param createAppSettingDto The app setting name and value.
+   */
+  updateAppSetting(createAppSettingDto: CreateAppSettingDto) {
+    return this._appSettingRepository.update(
+      { settingName: createAppSettingDto.settingName },
+      { settingValue: createAppSettingDto.settingValue }
+    );
   }
 
 }
