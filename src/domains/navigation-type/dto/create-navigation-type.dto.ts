a5ee93fcd32aecbc0558692d81268d814be2cfc9
Childish <cedric.belin1@gmail.com>
2026-05-06 13:01:33 +0200

    make thumbnail of nav type opti0onal

--- src/domains/navigation-type/dto/create-navigation-type.dto.ts
+++ src/domains/navigation-type/dto/create-navigation-type.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateNavigationTypeDto {
 
@@ -14,7 +14,7 @@ export class CreateNavigationTypeDto {
     @IsString()
     description: string;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsString()
-    thumbnailImage: string;
+    thumbnailImage?: string;
 }
