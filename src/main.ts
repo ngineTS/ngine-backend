885e14b177b8c5d9a88ced2726f374e4492feb0b
Childish <cedric.belin1@gmail.com>
2026-05-20 21:06:29 +0200

    fix cors url binding from env

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL,
     credentials: true
   });
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
