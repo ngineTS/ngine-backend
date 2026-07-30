ff2f3951b3b34abeda6175697cbf2ddf88bae2f8
Childish <cedric.belin1@gmail.com>
2026-07-30 18:52:14 +0200

    fix navigation validator service file

--- src/domains/navigation/navigation-validator.service.ts
+++ src/domains/navigation/navigation-validator.service.ts
@@ -31,7 +31,7 @@ export class NavigationValidatorService {
   ) {
     if (
       !userNavigationPermissions.find(obj => 
-        obj.navigationId === createNavigationDto.parentId && obj.permissionName.includes('add')
+        obj.navigationGroupId === createNavigationDto.parentGroupId && obj.permissionName.includes('add')
       )
     ) {
       throw new ForbiddenException();
@@ -53,25 +53,25 @@ export class NavigationValidatorService {
     updateNavigationDto: UpdateNavigationDto,
     userNavigationPermissions: NavigationPermissions
   ) {
+    const dbNavigation = await this._navigationRepository.findOne({
+      where: { id: id }
+    });
+    if (!dbNavigation) {
+      throw new NotFoundException(`Navigation with id ${id} doesn't exist.`);
+    }
+
     if (
       !userNavigationPermissions.find(
-        obj => obj.navigationId === id && obj.permissionName.includes('edit')
+        obj => obj.navigationGroupId === dbNavigation.groupId && obj.permissionName.includes('edit')
       )
     ) {
       throw new ForbiddenException();
     }
 
-    const dbNavigation = await this._navigationRepository.findOne({
-      where: { id: id }
-    });
-    if (!dbNavigation) {
-      throw new NotFoundException();
-    }
-
-    if ('parentId' in updateNavigationDto && updateNavigationDto.parentId !== dbNavigation.parentId) {
+    if ('parentGroupId' in updateNavigationDto && updateNavigationDto.parentGroupId !== dbNavigation.parentGroupId) {
       if(
         !userNavigationPermissions.find(obj => 
-        obj.navigationId === updateNavigationDto.parentId && obj.permissionName.includes('add')
+        obj.navigationGroupId === updateNavigationDto.parentGroupId && obj.permissionName.includes('add')
       )) {
         throw new ForbiddenException();
       }
@@ -92,7 +92,7 @@ export class NavigationValidatorService {
     navigations.forEach(navigation => {
       if (
         !userNavigationPermissions.find(obj =>
-          obj.navigationId === navigation.id && obj.permissionName.includes('edit')
+          obj.navigationGroupId === navigation.groupId && obj.permissionName.includes('edit')
         )
       ) {
         throw new ForbiddenException();
@@ -114,7 +114,7 @@ export class NavigationValidatorService {
   ) {
     if (
       !userNavigationPermissions.find(obj => 
-        obj.navigationId === navigation.id && obj.permissionName.includes('delete')
+        obj.navigationGroupId === navigation.groupId && obj.permissionName.includes('delete')
       )
     ) {
       throw new ForbiddenException();
@@ -130,12 +130,12 @@ export class NavigationValidatorService {
    * 
    * @param navigationDto The navigation to insert or update.
    * @param navigationId The navigation id (optional).
-   * @throws {NotFoundException} If `navigationDto.parentId` is not found in the database.
+   * @throws {NotFoundException} If `navigationDto.parentGroupId` is not found in the database.
    * @throws {NotFoundException} If `navigationDto.navigationTypeId` is not found in the database.
    * @throws {BadRequestException} If `navigationDto` type is a button and parent is not a menu or a redirect-button.
    * @throws {BadRequestException} If `navigationDto` type is a component and parent is not a dialog-button or a redirect-button without nav bar.
    * @throws {BadRequestException} If `navigationDto.name` is already used by sister navigations.
-   * @throws {BadRequestException} If `navigationDto.parentId` is equal to `navigationId`.
+   * @throws {BadRequestException} If `navigationDto.parentGroupId` is equal to `navigationGroupId`.
    * @throws {BadRequestException} If `navigationDto` parent is found in the descendants of navigation to update.
    * @throws {BadRequestException} If `navigationDto.showIconOnly` is true and `navigationDto.icon` is empty.
    */
@@ -143,10 +143,10 @@ export class NavigationValidatorService {
     navigationDto: UpdateNavigationDto,
     navigationId?: string
   ) {
-    if(navigationDto.parentId) {
+    if (navigationDto.parentGroupId) {
       let parentNavigation = await this._navigationRepository.findOne({
-        where: { 
-          id: navigationDto.parentId,
+        where: {
+          groupId: navigationDto.parentGroupId,
           deletedDate: IsNull(),
         },
         relations: [
@@ -157,7 +157,7 @@ export class NavigationValidatorService {
       });
     
       if (!parentNavigation) {
-        throw new NotFoundException(`Parent ${navigationDto.parentId} doesn't exist.`)
+        throw new NotFoundException(`Parent with group id ${navigationDto.parentGroupId} doesn't exist.`)
       }
       parentNavigation.children = parentNavigation.children.filter(child => !child.deletedDate);
 
@@ -211,11 +211,16 @@ export class NavigationValidatorService {
       }
 
       if (navigationId) {
-        if (navigationId === navigationDto.parentId) {
+        const dbNavigation = await this._navigationRepository.findOneBy({ id: navigationId });
+        if (!dbNavigation) {
+          throw new NotFoundException(`Navigation with id ${navigationId} is not found.`);
+        }
+
+        if (dbNavigation.groupId === navigationDto.parentGroupId) {
           throw new BadRequestException('Parent cannot be same navigation');
         }
 
-        await this.checkIfIsADescendant(navigationId, navigationDto.parentId);
+        await this.checkIfIsADescendant(dbNavigation.groupId, navigationDto.parentGroupId);
       }
 
       if (navigationDto.showIconOnly && !navigationDto.icon) {
@@ -229,28 +234,28 @@ export class NavigationValidatorService {
   /**
    * Check if given navigation is in descendants of other navigation.
    * 
-   * @param navigationId The navigation with descendants.
-   * @param parentId The navigation to check.
+   * @param navigationGroupId The navigation with descendants.
+   * @param parentGroupId The navigation to check.
    */
-  async checkIfIsADescendant(navigationId: string, parentId: string) {
+  async checkIfIsADescendant(navigationGroupId: string, parentGroupId: string) {
 
     const rows = await this._navigationRepository.query(
       `
       WITH RECURSIVE descendants AS (
-        SELECT id
+        SELECT "groupId"
         FROM ${process.env.DB_SCHEMA}.navigation
-        WHERE "parentId" = $1
+        WHERE "parentGroupId" = $1
         UNION ALL
-        SELECT n.id
+        SELECT n."groupId"
         FROM ${process.env.DB_SCHEMA}.navigation n
-        JOIN descendants d ON n."parentId" = d.id
+        JOIN descendants d ON n."parentGroupId" = d."groupId"
       )
       SELECT 1
       FROM descendants
       WHERE id = $2
       LIMIT 1
       `,
-      [navigationId, parentId],
+      [navigationGroupId, parentGroupId],
     );
 
     if (rows.length) {
