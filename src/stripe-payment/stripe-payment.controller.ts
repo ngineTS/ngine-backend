d6a5f2c49abca6ec6db2178dc9653792c7d92483
Childish <cedric.belin1@gmail.com>
2026-09-15 16:16:08 +0200

    add ref

--- src/stripe-payment/stripe-payment.controller.ts
+++ src/stripe-payment/stripe-payment.controller.ts
@@ -2,34 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { StripePaymentService } from './stripe-payment.service';
 import { UpdateStripePaymentDto } from './dto/update-stripe-payment.dto';
 import { Public } from 'src/core/auth/auth.guard';
+import { UserId } from 'src/core/decorators/user.decorator';
 
 @Controller('stripe-payment')
 export class StripePaymentController {
   constructor(private readonly stripePaymentService: StripePaymentService) {}
 
   @Public()
-  @Get('create-checkout-session/:priceId')
-  create(@Param('priceId') priceId: string) {
-    return this.stripePaymentService.createCheckoutSession(priceId);
+  @Get('create-checkout-session/:priceId/:roleId')
+  create(
+    @Param('priceId') priceId: string,
+    @Param('roleId') roleId: string,
+    @UserId() userId: string
+  ) {
+    return this.stripePaymentService.createCheckoutSession(priceId, userId, roleId);
   }
 
-  @Get()
-  findAll() {
-    return this.stripePaymentService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.stripePaymentService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStripePaymentDto: UpdateStripePaymentDto) {
-    return this.stripePaymentService.update(+id, updateStripePaymentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.stripePaymentService.remove(+id);
-  }
 }
--- src/stripe-payment/stripe-payment.service.ts
+++ src/stripe-payment/stripe-payment.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStripePaymentDto } from './dto/create-stripe-payment.dto';
-import { UpdateStripePaymentDto } from './dto/update-stripe-payment.dto';
 import Stripe = require('stripe');
+import { Request, Response } from 'express';
 
 @Injectable()
 export class StripePaymentService {
 
-  
   constructor() {
     this.stripe = new Stripe(
       'test-k',
@@ -18,36 +16,21 @@ export class StripePaymentService {
   successUrl = "http://localhost:4200/success";
   cancelUrl = "http://localhost:4200/cancel"
 
-  async createCheckoutSession(priceId: string) {
+  async createCheckoutSession(
+    priceId: string,
+    userId: string,
+    roleId: string
+  ) {
     const session = await this.stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [{price: priceId, quantity: 1 }],
+      client_reference_id: userId,
+      metadata: { roleId: roleId },
       mode: 'payment',
       success_url: this.successUrl,
       cancel_url: this.cancelUrl,
     });
 
     return { url: session.url };
-
-  }
-
-  create(createStripePaymentDto: CreateStripePaymentDto) {
-    return 'This action adds a new stripePayment';
-  }
-
-  findAll() {
-    return `This action returns all stripePayment`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} stripePayment`;
-  }
-
-  update(id: number, updateStripePaymentDto: UpdateStripePaymentDto) {
-    return `This action updates a #${id} stripePayment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stripePayment`;
   }
 }
